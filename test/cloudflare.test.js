88253bd4bd7cde79ea2659cdbf56de9286145d0c
Patrick Heneise <patrick@zentered.co>
2022-04-11 21:33:40 +0300

    fix: run tests only with environment variables

--- test/cloudflare.test.js
+++ test/cloudflare.test.js
@@ -5,8 +5,18 @@ import t from 'tap'
 import fn from '../cloudflare.js'
 
 const test = t.test
+const skip = process.env.CI
 
-test('put', async (t) => {
+test(
+  'skip tests on ci as env variables are require',
+  { skip: !skip },
+  async (t) => {
+    t.ok(true)
+    return
+  }
+)
+
+test('put', { skip: skip }, async (t) => {
   const testObj = { hello: 'world' }
 
   const accountId = process.env.CLOUDFLARE_ACCOUNT_ID
@@ -31,7 +41,7 @@ test('put', async (t) => {
   t.same(actual, null)
 })
 
-test('get', async (t) => {
+test('get', { skip: skip }, async (t) => {
   const accountId = process.env.CLOUDFLARE_ACCOUNT_ID
   const apiKey = process.env.CLOUDFLARE_API_KEY
   const accountEmail = process.env.CLOUDFLARE_ACCOUNT_EMAIL
